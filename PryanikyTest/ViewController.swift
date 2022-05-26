916c75bdba5e8683b49bb37c3f78072ca8686b87
SemjonG <simon.gerasimov@gmail.com>
2022-05-26 23:15:55 +0300

    Initial Commit

---
+++ PryanikyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PryanikyTest
+//
+//  Created by mac on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
